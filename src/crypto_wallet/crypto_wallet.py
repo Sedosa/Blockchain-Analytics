8aaa87bfa6695df0af0c9c79a977de153a5c491a
Alastair James <ali_j692@hotmail.com>
2021-06-01 20:46:38 +0100

    Archiving unnecessary code

--- src/crypto_wallet/crypto_wallet.py
+++ src/crypto_wallet/crypto_wallet.py
@@ -8,6 +8,7 @@ def parse_json(json_path):
     with open(json_path) as j:
         crypto_dict = json.load(j)
     return crypto_dict
+    
 def get_price(crypto):
     API_ENDPOINT = f"https://min-api.cryptocompare.com/data/price?fsym={crypto}&tsyms=GBP"
     response = requests.get(API_ENDPOINT,headers=HEADER)
@@ -19,7 +20,7 @@ def main(json_path):
     wallet_dict = dict()
     for key_ in crypto_dict.keys():
         price = get_price(key_)
-        print(f"{key_}: £{price*crypto_dict[key_]}")
+        print(f"{key_}: £{round(price*crypto_dict[key_],2)}")
         wallet_dict[key_] = price*crypto_dict[key_]
 
     print(f"Total: £{sum(wallet_dict.values())}")
